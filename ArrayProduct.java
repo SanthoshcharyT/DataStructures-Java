a9f87d60d4c90e2c5d077b9f0abd59dd86b9f351
Santhosh Chary <151213795+SanthoshcharyT@users.noreply.github.com>
2026-06-21 22:47:16 +0530

    Product of array

---
+++ ArrayProduct.java
@@ -0,0 +1,26 @@
+public class ArrayProduct {
+    public static int[] arrayProduct(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    result[i] *= arr[j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4};
+        int[] result = arrayProduct(arr);
+
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+}
