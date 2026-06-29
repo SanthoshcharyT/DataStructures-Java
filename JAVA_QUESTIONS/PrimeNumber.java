8226ddb0f66201072cd63a46a582261fe33bd68e
Santhosh Chary <151213795+SanthoshcharyT@users.noreply.github.com>
2026-06-29 11:29:32 +0530

    Primenumber

--- JAVA_QUESTIONS/PrimeNumber.java
+++ JAVA_QUESTIONS/PrimeNumber.java
@@ -1,6 +1,6 @@
 public class PrimeNumber {
     public static void main(String[] args) {
-        int num = 56;
+        int num = 1;
         if(num % 2 != 0){
             System.out.println(num + " is a Prime Number");
         }else{
